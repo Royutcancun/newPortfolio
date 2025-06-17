978926f2dfa2c3f74090ce5f6c0c6347e96ccb29
Rogelio Lopez <royutcancun@gmail.com>
2025-06-17 13:50:48 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { FooterComponent } from "./footer/footer.component";
+import { WorkComponent } from "./sections/work/work.component";
+import { PortfolioComponent } from "./sections/portfolio/portfolio.component";
+import { AboutComponent } from "./sections/about/about.component";
+import { ContactComponent } from "./sections/contact/contact.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NavbarComponent, FooterComponent, WorkComponent, PortfolioComponent, AboutComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { WorkComponent } from './sections/work/work.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: './header.component.ts',
+    component: HeaderComponent,
+
+  },
+  {
+    path: './navbar.component.ts',
+    component: NavbarComponent
+  },
+  {
+    path: './sections/work/work.component.ts',
+    component: WorkComponent
+  }
+];
